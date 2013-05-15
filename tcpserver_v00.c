364d9e4f412798f3eda8365d57b27c596de8b3f9
hhangelmm <xuemin.cheng@gmail.com>
2013-05-15 19:12:13 +0800

    Update tcpserver_v00.c

--- tcpserver_v00.c
+++ tcpserver_v00.c
@@ -17,15 +17,12 @@
 
 int main(void)
 {
-//	daemon(0,0);
-	//signal(SIGCLD, SIG_IGN);
 	struct sockaddr_in servaddr,cliaddr;
 	socklen_t cliaddr_len;
 	int listenfd,connfd;
 	char buf[MAXLINE];
 	char str[INET_ADDRSTRLEN];
 	int n,num=0;
-	//int i;
 	listenfd = socket(AF_INET,SOCK_STREAM,0);
 	bzero(&servaddr,sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
@@ -44,24 +41,13 @@ int main(void)
 			exit(0);
 			}
 		}
-		/*i = fork();
-		if(i == -1){
-			printf("fork error");
-			exit(1);
-		}else if(i == 0){
-			close(listenfd);*/
-			inet_ntop(AF_INET, &cliaddr.sin_addr, str,sizeof(str));
-			while((n = read(connfd,buf,MAXLINE))>0){
-				num++;
-				printf("received from at PORT %d,%d\n",ntohs(cliaddr.sin_port),num);
-				write(connfd,buf,n);
-			}
-			printf("the client has been closed.\n");
-			close(connfd);
-			exit(0);
-		/*}else
-		{
-			close(connfd);
-		}*/
+		inet_ntop(AF_INET, &cliaddr.sin_addr, str,sizeof(str));
+		while((n = read(connfd,buf,MAXLINE))>0){
+			num++;
+			printf("received from at PORT %d,%d\n",ntohs(cliaddr.sin_port),num);
+			write(connfd,buf,n);
+		}
+		printf("the client has been closed.\n");
+		close(connfd);
 	}
 }
